4074167217b5e0312ffb7e522d5aa99635e1a1f7
adamascencio <ascencioadam@gmail.com>
2022-09-06 16:53:27 -0700

    remove unnecessary api module

--- src/utilities/google-api.js
+++
@@ -1,9 +0,0 @@
-import fetch from 'node-fetch';
-
-const BASE_URL = `https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyDYbTfV1c52W3gLMwxvOZQgEBBSb2aj4og&components=postal_code:`;
-
-export async function getLatLong(zipCode) {
-  const response = await fetch(`${BASE_URL}${zipCode}`);
-  const data = await response.json();
-  return data.results[0].geometry.location;
-}
