5d685d2ab01d3eb87bc6755865906558abcb486d
adamascencio <ascencioadam@gmail.com>
2022-09-07 22:18:57 -0700

    render markers for basketball courts within a 15 mile radius

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from "react";
-import LocationPin from "../LocationPin/LocationPin";
+// import LocationPin from "../LocationPin/LocationPin";
 
 export default function Map({ user }) {
   const defaultProps = {
@@ -14,32 +14,49 @@ export default function Map({ user }) {
 
   useEffect(function() {
     if (user.lat && user.long) {
+      // initialize the map
       const map = new window.google.maps.Map(
         mapDiv.current, {
           zoom: defaultProps.zoom,
           center: defaultProps.center,
         }
       );
+      
       new window.google.maps.Marker({ position: defaultProps.center, map: map });
+
+      // get nearby basketball courts
+      const request = {
+        location: defaultProps.center,
+        radius: '24140.2', // 15 miles
+        query: 'basketball court',
+        type: ['gym', 'park']
+      };
+
+      const places = new window.google.maps.places.PlacesService(map);
+      places.textSearch(request, callback);
+
+      // add pins on map for each nearby basketball court
+      nearbyLocations.map(place => {
+        return new window.google.maps.Marker({
+          position: { lat: place.geometry.location.lat(), lng: place.geometry.location.lng() },
+          map,
+          title: place.name
+        });
+      });
     }
   }, []);
 
-  function findNearbyLocations() {
-    const service = new window.google.maps.places.PlacesService(document.createElement('div'));
-    const request = {
-      location: { lat: user.lat, lng: user.long },
-      radius: '500',
-      type: ['gym', 'park', 'school', 'university']
-    };
-    service.nearbySearch(request, (results, status) => {
-      if (status === window.google.maps.places.PlacesServiceStatus.OK) {
-        setNearbyLocations(results);
-      }
-    });
+  function callback(results, status) {
+    if (status === window.google.maps.places.PlacesServiceStatus.OK) {
+      setNearbyLocations(results);
+      console.log('results: ', results);
+      console.log('nearby courts', nearbyLocations);
+      console.log('status: ', status);
+    }
   }
 
+
   return (
-    findNearbyLocations(),
     <div className="s2" style={{ height: '40vh', width: '100%' }} ref={mapDiv}>
   
     </div>
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -10,7 +10,6 @@ import './App.css';
 
 export default function App() {
   const [user, setUser] = useState(getUser());
-  console.log(user);
 
   return (
     <main className="App">
