9a14c5d60087098d48315a0853c955081e12ac6a
adamascencio <ascencioadam@gmail.com>
2022-12-10 16:08:08 -0800

    test workflow update

--- src/components/RunCreateForm/RunCreateForm.jsx
+++ src/components/RunCreateForm/RunCreateForm.jsx
@@ -13,7 +13,6 @@ export default function RunCreateForm({ modelLocation, nearbyLocations, todayStr
     amPm: 'AM'
   });
   let navigate = useNavigate();
-  console.log(formData);
   // const TIMES = [
   //   0, 1, 2, 3, 4, 5, 6, 7, 
   //   8, 9, 10, 11, 12, 13, 14, 15, 
