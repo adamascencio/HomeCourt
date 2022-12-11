03fa56269494012971c66c0184f5093f9185bba2
adamascencio <ascencioadam@gmail.com>
2022-12-10 16:03:19 -0800

    add workflow

--- src/components/RunCreateForm/RunCreateForm.jsx
+++ src/components/RunCreateForm/RunCreateForm.jsx
@@ -14,17 +14,17 @@ export default function RunCreateForm({ modelLocation, nearbyLocations, todayStr
   });
   let navigate = useNavigate();
   console.log(formData);
-  const TIMES = [
-    0, 1, 2, 3, 4, 5, 6, 7, 
-    8, 9, 10, 11, 12, 13, 14, 15, 
-    16, 17, 18, 19, 20, 21, 22, 23
-  ];
-  const runSchedule = {
-    schedule: {
-      date: new Date(formData.date),
-      hoursAvail: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
-    }
-  }
+  // const TIMES = [
+  //   0, 1, 2, 3, 4, 5, 6, 7, 
+  //   8, 9, 10, 11, 12, 13, 14, 15, 
+  //   16, 17, 18, 19, 20, 21, 22, 23
+  // ];
+  // const runSchedule = {
+  //   schedule: {
+  //     date: new Date(formData.date),
+  //     hoursAvail: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
+  //   }
+  // }
 
   function getGooglePhotoSrc(court) {
     return court.photos ? court.photos[0].getUrl({maxWidth: 400, maxHeight: 400}) : '';
